73064d534384a1c0a8901c64f5f4e15de76152d8
Edward Buckle <edward.buckle0@gmail.com>
2019-10-02 14:10:31 +0100

    npm updating

--- test/src/license.test.js
+++ test/src/license.test.js
@@ -16,32 +16,36 @@ const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const fs = require('fs-extra');
 const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 PerformTests();
 
 function PerformTests() {
-    for (const key in allPackageJsonPaths) {
-        let packageJsonPath = null;
-        let packageJson = null;
-        let results = {};
-    
-        describe(`${key} - perform license check`, function() {
-            this.timeout(240000);
-            before(async function() {
-                packageJsonPath = allPackageJsonPaths[key];
-                packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
-                results = await performCheck(packageJson);
-            });
-
-            describe('examine results', function() {
-                it('should not contain uncleared licenses', function() {
-                    chai.expect(results['unclearedPackages']).to.be.deep.equals({});
-                }); 
-    
-                it('should not have open source unfriendly packages', function() {
-                    chai.expect(results['problemPackages']).to.be.deep.equals({});
+    describe('perform license check on package.json', function() {
+        this.timeout(240000);
+        before(async function() {
+            await exec('npm install https://github.com/ebuckle/codewind-license-checker.git');
+        });
+        for (const key in allPackageJsonPaths) {
+            let packageJsonPath = null;
+            let packageJson = null;
+            let results = {};
+            describe(`${key}`, function() {
+                before(async function() {
+                    packageJsonPath = allPackageJsonPaths[key];
+                    packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+                    results = await performCheck(packageJson);
+                });
+                describe('examine results', function() {
+                    it('should not contain uncleared licenses', function() {
+                        chai.expect(results['unclearedPackages']).to.be.deep.equals({});
+                    }); 
+        
+                    it('should not have open source unfriendly packages', function() {
+                        chai.expect(results['problemPackages']).to.be.deep.equals({});
+                    });
                 });
             });
-        });
-    }
+        }
+    });
 }
