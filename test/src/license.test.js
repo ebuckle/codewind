5af5dcd01a78e31135f50ba23de8f0130c012984
Edward Buckle <edward.buckle0@gmail.com>
2019-10-03 10:40:45 +0100

    removal of assertions, console logging

--- test/src/license.test.js
+++ test/src/license.test.js
@@ -18,34 +18,21 @@ const fs = require('fs-extra');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-PerformTests();
-
-function PerformTests() {
-    describe('perform license check on package.json', function() {
-        this.timeout(240000);
-        before(async function() {
-            await exec('npm install https://github.com/ebuckle/codewind-license-checker.git');
-        });
-        for (const key in allPackageJsonPaths) {
-            let packageJsonPath = null;
-            let packageJson = null;
-            let results = {};
-            describe(`${key}`, function() {
-                before(async function() {
-                    packageJsonPath = allPackageJsonPaths[key];
-                    packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
-                    results = await performCheck(packageJson);
-                });
-                describe('examine results', function() {
-                    it('should not contain uncleared licenses', function() {
-                        chai.expect(results['unclearedPackages']).to.be.deep.equals({});
-                    }); 
-        
-                    it('should not have open source unfriendly packages', function() {
-                        chai.expect(results['problemPackages']).to.be.deep.equals({});
-                    });
-                });
-            });
-        }
+describe('perform license check on package.json', function() {
+    this.timeout(240000);
+    before(async function() {
+        await exec('npm install https://github.com/ebuckle/codewind-license-checker.git');
     });
-}
+    for (const key in allPackageJsonPaths) {
+        let packageJsonPath = null;
+        let packageJson = null;
+        let results = {};
+        describe(`${key}`, async function() {
+            packageJsonPath = allPackageJsonPaths[key];
+            packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+            results = await performCheck(packageJson);
+            delete results.acceptedPackages;
+            console.log(results);
+        });
+    }
+});
